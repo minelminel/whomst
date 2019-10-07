ef7577e7e7f88123719b1249d607bdbb41411321
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-10-07 01:46:45 -0400

    bumped version and added setup details

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    version="0.0.1",
+    version="0.0.2",
     name="whomst",
+    author="minelminel",
+    short_description="infer Python package requirements",
     packages=['whomst',],
     entry_points={
         "console_scripts": [
