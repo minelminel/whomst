3de472b21ab55d4b72d9d01b5af1856c73b93035
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-10-15 18:00:09 -0400

    updated packaging scripts, added license and travis deployment

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 import os
-
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     version="0.0.2",
     name="whomst",
     author="minelminel",
+    author_email="ctrlcmdspace@gmail.com",
     short_description="infer Python package requirements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/minelminel/whomst",
     packages=['whomst',],
     entry_points={
         "console_scripts": [
             "whomst=whomst:main"
-        # "name_of_executable = module.with:function_to_execute"
         ]
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">3.0",
+    license="MIT",
 )
